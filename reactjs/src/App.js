cd85280d2b3d91c05a386952b7b80d7aa0e328ce
mdpabel <mdpabel385@gmail.com>
2021-01-05 15:59:04 +0600

    react web workers

---
+++ reactjs/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExpensiveCal from "./components/ExpensiveCal";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpensiveCal />
+    </div>
+  );
+}
+
+export default App;
